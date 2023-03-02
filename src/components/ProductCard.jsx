2f61dd73e5993d2904e65eedf85ff9fa3f754c04
Maverick Seneris <mavseneris@gmail.com>
2023-03-02 17:20:01 +0800

    deleted products page

--- src/components/ProductCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ProductCard({item}) {
-  return (
-    <div>
-      <img src = {item.img1}/>
-    </div>
-  )
-}
--- src/pages/ProductsaPage.jsx
+++ src/pages/ProductsaPage.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
-import All from '../components/All'
+
 
 
 export default function ProductsPage() {
   return (
     <div>
       <Navbar/>
-      <All/>
       <Footer/>
     </div>
   )

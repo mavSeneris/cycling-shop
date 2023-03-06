d3b8137c193b7a321659b9423d160c8e9b7ecf69
Maverick Seneris <mavseneris@gmail.com>
2023-03-06 11:50:33 +0800

    placed all productPage links to all category pages

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 export default function ProductCard({ item }) {
   return (
-      <div className='item-card-wrapper'>
+    <div className='item-card-wrapper'>
+      <a href={`product/${item.id}`}>
         <img className="item-card" src={item.img1} />
         <img className="item-card-hover" src={item.img2} />
         <span className='product-name'>{item.name}</span>
         <span className='product-price'>${item.price}.00 US</span>
-      </div>
+      </a>
+    </div>
   )
 }

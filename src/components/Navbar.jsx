88bf83e9b0494e9f5782d3996e20a7913b025c5c
Maverick Seneris <mavseneris@gmail.com>
2023-03-10 15:13:10 +0800

    added shopping bag feature with add, delete, total and subtotal function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { nanoid } from 'nanoid'
 
 
 export default function Navbar({ bag, setBag }) {
   const [showBag, setShowBag] = useState(false)
+  const [totalPrice, setTotalPrice] = useState(0)
 
   function toggleBag() {
     setShowBag(prevShowBag => !prevShowBag)
@@ -16,6 +17,13 @@ export default function Navbar({ bag, setBag }) {
     setBag(currentBagItems =>
       currentBagItems.filter(item => item.id !== itemId))
   }
+
+  useEffect(() => {
+    const initialTotalPrice = 0
+    const totalItemsPrice = bag.reduce((accumulator, current) =>
+      accumulator + current.price, initialTotalPrice)
+    setTotalPrice(totalItemsPrice)
+  })
 
   const bagItems = bag.map(item =>
     <div className="bag-item-container"
@@ -32,7 +40,7 @@ export default function Navbar({ bag, setBag }) {
         src={deleteIcon}
       /> */}
       <div className='delete-icon' onClick={(event) => deleteBagItem(event, item.id)}>
-      <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M360 184h-8c4.4 0 8-3.6 8-8v8h304v-8c0 4.4 3.6 8 8 8h-8v72h72v-80c0-35.3-28.7-64-64-64H352c-35.3 0-64 28.7-64 64v80h72v-72zm504 72H160c-17.7 0-32 14.3-32 32v32c0 4.4 3.6 8 8 8h60.4l24.7 523c1.6 34.1 29.8 61 63.9 61h454c34.2 0 62.3-26.8 63.9-61l24.7-523H888c4.4 0 8-3.6 8-8v-32c0-17.7-14.3-32-32-32zM731.3 840H292.7l-24.2-512h487l-24.2 512z"></path></svg>
+        <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" height="1.2em" width="1.2em" xmlns="http://www.w3.org/2000/svg"><path d="M360 184h-8c4.4 0 8-3.6 8-8v8h304v-8c0 4.4 3.6 8 8 8h-8v72h72v-80c0-35.3-28.7-64-64-64H352c-35.3 0-64 28.7-64 64v80h72v-72zm504 72H160c-17.7 0-32 14.3-32 32v32c0 4.4 3.6 8 8 8h60.4l24.7 523c1.6 34.1 29.8 61 63.9 61h454c34.2 0 62.3-26.8 63.9-61l24.7-523H888c4.4 0 8-3.6 8-8v-32c0-17.7-14.3-32-32-32zM731.3 840H292.7l-24.2-512h487l-24.2 512z"></path></svg>
       </div>
     </div>
   );
@@ -77,6 +85,32 @@ export default function Navbar({ bag, setBag }) {
       {showBag && bag.length > 0 && <div className='bag'>
         <h4>Your Shopping Bag</h4>
         <div className="bag-item-container-wrapper">{bagItems}</div>
+        <div className="items-total-price-container">
+          <div className="items-text-wrapper">
+            <p>Bag total:</p>
+            <p>Delivery</p>
+          </div>
+
+          <div className="items-text-wrapper">
+            <p>${totalPrice}.00</p>
+            <p>$4.59</p>
+          </div>
+        </div>
+        
+        <div className="items-total-price-container sub-total">
+          <div className="items-text-wrapper">
+            <h4>Subtotal:</h4>
+          </div>
+          <div className="items-text-wrapper">
+            <h4>{`$${totalPrice + 4.59}`}</h4>
+          </div>
+        </div>
+        <div className="button-center">
+          <button
+            className="checkout-button">
+            Proceed to Checkout
+          </button>
+        </div>
       </div>}
     </div>
   )

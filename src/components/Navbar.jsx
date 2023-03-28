2094315854d16a30f553896bf23e7682661d2f22
Maverick Seneris <mavseneris@gmail.com>
2023-03-28 13:47:42 +0800

    username in navbar now turn to initial when in mobile screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { nanoid } from 'nanoid'
 export default function Navbar({ bag, setBag, isLoggedIn, username, setUsername, setIsLoggedIn }) {
   const [showBag, setShowBag] = useState(false)
   const [totalPrice, setTotalPrice] = useState()
+  const [showMessage, setShowMessage] = useState(false);
 
 
   const location = useLocation()
@@ -71,6 +72,14 @@ export default function Navbar({ bag, setBag, isLoggedIn, username, setUsername,
       alert('Cancelled!')
     }
   }
+
+  const handleMouseEnter = () => {
+    setShowMessage(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowMessage(false);
+  };
 
   console.log(username)
 
@@ -150,14 +159,36 @@ export default function Navbar({ bag, setBag, isLoggedIn, username, setUsername,
               <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="1.48em" width="1.48em" xmlns="http://www.w3.org/2000/svg"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
             </Link>
           </div>
-        </div> : <div className='username'>{username}</div>}
+        </div> : <div className='username'>
+          {username}
+        </div>
+        }
+        {isLoggedIn &&
+          <div
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            onClick={handleLogOut}
+          >
+            <div className='user-initial'
+
+            >
+              {username[0].toUpperCase()}
+            </div>
+            {showMessage && (
+              <div className="logout-message">Click to log out.</div>
+            )}
+
+          </div>
+
+        }
+
         {isLoggedIn && <div className='logout-icon' onClick={handleLogOut}>
-          <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" height="1.48em" width="1.48em" xmlns="http://www.w3.org/2000/svg"><g><path fill="none" d="M0 0h24v24H0z"></path><path d="M5 22a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v3h-2V4H6v16h12v-2h2v3a1 1 0 0 1-1 1H5zm13-6v-3h-7v-2h7V8l5 4-5 4z"></path></g></svg>
+          <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" height="1.2em" width="1.4em" xmlns="http://www.w3.org/2000/svg"><g><path fill="none" d="M0 0h24v24H0z"></path><path d="M5 22a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v3h-2V4H6v16h12v-2h2v3a1 1 0 0 1-1 1H5zm13-6v-3h-7v-2h7V8l5 4-5 4z"></path></g></svg>
         </div>}
 
         {path !== "/" && <div className='shopping-cart' onClick={toggleBag}>
           <a href={'#'}>
-            <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" height="1.4em" width="1.4em" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M14 5H2v9a1 1 0 001 1h10a1 1 0 001-1V5zM1 4v10a2 2 0 002 2h10a2 2 0 002-2V4H1z" clipRule="evenodd"></path><path d="M8 1.5A2.5 2.5 0 005.5 4h-1a3.5 3.5 0 117 0h-1A2.5 2.5 0 008 1.5z"></path></svg>
+            <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" height="1.4em" width="1.2em" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M14 5H2v9a1 1 0 001 1h10a1 1 0 001-1V5zM1 4v10a2 2 0 002 2h10a2 2 0 002-2V4H1z" clipRule="evenodd"></path><path d="M8 1.5A2.5 2.5 0 005.5 4h-1a3.5 3.5 0 117 0h-1A2.5 2.5 0 008 1.5z"></path></svg>
           </a>
           {totalQuantity > 0 && <div className='item-count'>{totalQuantity}</div>}
         </div>}

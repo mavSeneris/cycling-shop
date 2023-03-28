fa12f750044752829cd54513870da4241d09b0fe
Maverick Seneris <mavseneris@gmail.com>
2023-03-28 17:55:23 +0800

    revise style of slide component

--- src/components/Slides.jsx
+++ src/components/Slides.jsx
@@ -5,7 +5,13 @@ import 'react-slideshow-image/dist/styles.css'
 const spanStyle = {
   padding: '20px',
   background: '#efefef',
-  color: '#000000'
+  color: '#000000',
+  backgroundColor: 'transparent',
+  color: 'white',
+  fontSize: '2.5rem',
+  fontWeight: '800',
+  letterSpacing: '3px',
+  fontStyle: 'italic',
 }
 
 const divStyle = {
@@ -13,48 +19,43 @@ const divStyle = {
   alignItems: 'center',
   justifyContent: 'center',
   backgroundSize: 'cover',
-  height: '80vh'
+  // backgroundRepeat: 'no-repeat',
+  // width: '100%',
+  height: '700px',
 }
 
-const responsiveSettings = [
-  {
-    breakpoint: 800,
-    settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1
-    },
-  },
-  {
-    breakpoint: 500,
-    settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1
-    }
-  }
-];
+
 const slideImages = [
   {
-    url: 'https://images.unsplash.com/photo-1631276893368-554b60393efb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-    caption: 'Slide 1'
+    url: 'https://images.prismic.io/maap/a22b8f56-0585-4142-b675-883adc1e66eb_OnBike_Man_Base_Layers.jpg?auto=compress,format',
+    caption: '//New Arrivals>>'
   },
   {
-    url: 'https://images.unsplash.com/photo-1616560448125-ee0fc3a352a9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTQwfHxjeWNsaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60',
-    caption: 'Slide 2'
+    url: 'https://images.prismic.io/maap/91ef534b-cb7a-49b3-9408-4405b6b37217_OnBike_Woman_Base_Layers.jpg?auto=compress,format',
+    caption: '//Shop Women>>'
   },
   {
-    url: 'https://images.unsplash.com/photo-1544011343-494fe72e6bab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-    caption: 'Slide 3'
+    url: 'https://images.prismic.io/maap/9089c7c2-596e-42ee-bac8-5abbdf3c98ed_W_ContentBlock_LSJerseys.jpg?auto=compress,format',
+    caption: '//Archive Sale>>'
   },
+  {
+    url: 'https://images.prismic.io/maap/29d5d8b3-6c87-491d-9928-51680f84b127_M_ContentBlock_Jerseys.jpg?auto=compress,format',
+    caption: '//shop men>>'
+  },
+  {
+    url: 'https://images.prismic.io/maap/a64bf36d-3f1e-4f22-b595-935557c3e167_OnBike_Man_LS_Tees.jpg?auto=compress,format',
+    caption: '//our story>>'
+  }
 ];
 
 const Slides = () => {
   return (
     <div className="slide-container" >
-      <Slide responsive={responsiveSettings}>
+      <Slide className='slide'>
         {slideImages.map((slideImage, index) => (
           <div key={index}>
-            <div style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
-
+            <div className='slide-image' style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
+              <span style={spanStyle} className='slide-caption'>{slideImage.caption.toUpperCase()}</span>
             </div>
           </div>
         ))}

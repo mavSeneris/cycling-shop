335e575db82a3843e0a6911b973264106dfe5512
Maverick Seneris <mavseneris@gmail.com>
2023-03-28 19:16:53 +0800

    revis home page styles

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -28,7 +28,7 @@ export default function Home() {
       {/* Render a welcome message and image only when the path is '/' */}
       {path === "/" && <div className='welcome'>
         <h2 className='welcome-title'>PURSUE A NEW DIMENSION</h2>
-        <img className='welcome-img' src="https://cdn.shopify.com/s/files/1/0419/5939/1399/files/4qChoZT_2048x2048.jpg?v=1601115655" alt="Welcome" />
+        <img className='welcome-img' src="https://scontent.fmnl25-3.fna.fbcdn.net/v/t1.6435-9/174260825_1897406373776610_5815923568521374055_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=2c4854&_nc_eui2=AeGQmMD3uXcKqdVyoQK8Wl0l8eqp7-_wRXjx6qnv7_BFeK5REv_r87PdLbS8Gf_xrXY&_nc_ohc=gO7sjmxq0mYAX-8pBPY&_nc_ht=scontent.fmnl25-3.fna&oh=00_AfC59HNyFY-xZZwYayOHyg2_01b2W6sExe2MhT2dNkPXdA&oe=644A1E6B" alt="Welcome" />
         <span className='shop-now'>
           {/* Render a link to '/home' and a logo */}
           <Link to={'/home'}>
